e1f77edd697c4e5c16d4d2275b0c98b810663b08
baron.br <baron.br@7ac2fbcf-ae54-0410-8a8d-775b4cb24fee>
2008-10-14 00:54:23 +0000

    creating a timecard (almost)

---
+++ timetracker/timetracker-core/sql/db-load.sql
@@ -0,0 +1,6 @@
+insert into timetracker.tt_user (id, first_name, last_name, username) values (2000, 'system', 'user', 'user'); 
+insert into timetracker.tt_user (id, first_name, last_name, username) values (2001, 'system', 'admin', 'admin'); 
+insert into timetracker.tt_user (id, first_name, last_name, username) values (2002, 'Naresh', 'Bhatia', 'nbhatia'); 
+insert into timetracker.tt_user (id, first_name, last_name, username) values (2003, 'Louis', 'Coude', 'lcoude'); 
+insert into timetracker.tt_user (id, first_name, last_name, username) values (2004, 'Eric', 'Crutchfield', 'ecrutchfield'); 
+insert into timetracker.tt_user (id, first_name, last_name, username) values (2005, 'Chris', 'Micali', 'cmicali'); 
