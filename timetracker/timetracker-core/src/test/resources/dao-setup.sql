7e15f554af77cd7f13b5f12c14f04ab1781c495d
baron.br <baron.br@7ac2fbcf-ae54-0410-8a8d-775b4cb24fee>
2008-10-17 00:57:59 +0000

    new timecard setup

--- timetracker/timetracker-core/src/test/resources/dao-setup.sql
+++
@@ -1,3 +0,0 @@
-insert into tt_user (id, first_name, last_name, username) values (-1, 'first1', 'last1', 'user1');
-insert into tt_timecard (id, status, start_date, comments, approver_id, submitter_id) 
-values (-1, 'Draft',     '2006/06/05', 'Timecard -1', null, -1);
--- timetracker/timetracker-core/src/test/resources/dao-teardown.sql
+++
@@ -1,2 +0,0 @@
-delete from tt_timecard where id = -1;
-delete from tt_user where id = -1;
