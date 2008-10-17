0b3112e1c4630bea39467f0000f135493eb2646d
baron.br <baron.br@7ac2fbcf-ae54-0410-8a8d-775b4cb24fee>
2008-10-17 20:42:57 +0000

    working on TimeAllocation

--- trunk/timetracker/timetracker-core/src/main/java/com/bbaron/timetracker/model/IEntity.java
+++ trunk/timetracker/timetracker-core/src/main/java/com/bbaron/timetracker/model/IEntity.java
@@ -2,7 +2,7 @@ package com.bbaron.timetracker.model;
 
 import java.io.Serializable;
 
-public interface IEntity<PK extends Serializable> {
+public interface IEntity<PK extends Serializable> extends Serializable {
 
 	void setId(PK id);
 	PK getId();
