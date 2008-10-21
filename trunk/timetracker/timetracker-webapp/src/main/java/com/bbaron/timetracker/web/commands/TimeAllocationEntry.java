6fa3c63abf4b3c149d656cbb29a7743af1fbc23d
baron.br <baron.br@7ac2fbcf-ae54-0410-8a8d-775b4cb24fee>
2008-10-21 21:13:49 +0000

    working on enter Time alloc use case

---
+++ trunk/timetracker/timetracker-webapp/src/main/java/com/bbaron/timetracker/web/commands/TimeAllocationEntry.java
@@ -0,0 +1,56 @@
+package com.bbaron.timetracker.web.commands;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.bbaron.timetracker.model.Task;
+import com.bbaron.timetracker.model.TimeAllocation;
+
+public class TimeAllocationEntry implements Serializable {
+
+	private static final long serialVersionUID = 7960060982571986009L;
+	private TimeAllocation alloc = new TimeAllocation();
+	private Long timecardId;
+
+	public TimeAllocation getTimeAllocation() {
+		return alloc;
+	}
+	
+	public Long getTimecardId() {
+		return timecardId;
+	}
+
+	public void setTimecardId(Long timecardId) {
+		this.timecardId = timecardId;
+	}
+
+	public Task getTask() {
+		return alloc.getTask();
+	}
+
+	public Date getTimePeriodEndTime() {
+		return alloc.getTimePeriodEndTime();
+	}
+
+	public Date getTimePeriodStartTime() {
+		return alloc.getTimePeriodStartTime();
+	}
+
+	public void setTask(Task task) {
+		alloc.setTask(task);
+	}
+
+	public void setTimePeriodEndTime(Date timePeriodEndTime) {
+		alloc.setTimePeriodEndTime(timePeriodEndTime);
+	}
+
+	public void setTimePeriodStartTime(Date timePeriodStartTime) {
+		alloc.setTimePeriodStartTime(timePeriodStartTime);
+	}
+
+	@Override
+	public String toString() {
+		return alloc.toString();
+	}
+
+}
